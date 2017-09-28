e56e3b4b13e075e31f05dd37f96b64967b985470
Jarno Elovirta <jarno.elovirta@wunderdog.fi>
2017-09-28 20:58:26 +0300

    Add missing HTML entity class

---
+++ src/main/java/com/elovirta/dita/markdown/renderer/Entities.java
@@ -0,0 +1,20 @@
+package com.elovirta.dita.markdown.renderer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Entities {
+
+    public static final Properties ENTITIES;
+    static {
+        ENTITIES = new Properties();
+        try (InputStream in = Entities.class.getResourceAsStream("/entities.properties")) {
+            ENTITIES.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
