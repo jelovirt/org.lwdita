a91e36aa7860f10e22afdc1ebf390819db215bc5
Jarno Elovirta <jarno@elovirta.com>
2023-02-24 16:06:18 +0200

    Add generative task test
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/test/java/com/elovirta/dita/markdown/SpecializeFilterTest.java
+++ src/test/java/com/elovirta/dita/markdown/SpecializeFilterTest.java
@@ -2,6 +2,8 @@ package com.elovirta.dita.markdown;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
@@ -19,6 +21,12 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -67,5 +75,124 @@ public class SpecializeFilterTest {
             }
             assertFalse(diff.hasDifferences());
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("taskArgument")
+    public void task(int context, int info, int inline) throws Exception {
+        final SpecializeFilter filter = new SpecializeFilter();
+        filter.setParent(parserFactory.newSAXParser().getXMLReader());
+        final Transformer transformer = transformerFactory.newTransformer();
+        final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        final Document act = documentBuilder.newDocument();
+        try (Reader srcIn = generateSrc(context, info, inline);
+             Reader expIn = generateExp(context, info, inline)) {
+            transformer.transform(new SAXSource(filter, new InputSource(srcIn)), new DOMResult(act));
+            final Document exp = documentBuilder.parse(new InputSource(expIn));
+            final Diff diff = DiffBuilder
+                    .compare(act)
+                    .withTest(exp)
+                    .normalizeWhitespace()
+                    .ignoreWhitespace()
+                    .checkForIdentical()
+                    .build();
+            if (diff.hasDifferences()) {
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.transform(new DOMSource(act), new StreamResult(System.out));
+            }
+            assertFalse(diff.hasDifferences());
+        }
+    }
+
+    private static Stream<Arguments> taskArgument() {
+        final List<Arguments> buf = new ArrayList<>();
+        for (int context = 0; context < 3; context++) {
+            for (int info = 0; info < 3; info++) {
+                for (int inline = 0; inline < 3; inline++) {
+                    buf.add(Arguments.of(context, info, inline));
+                }
+            }
+        }
+        return buf.stream();
+    }
+
+    private Reader generateSrc(int context, int info, int inline) {
+        final StringBuilder buf = new StringBuilder();
+        buf.append("<topic xmlns:ditaarch='http://dita.oasis-open.org/architecture/2005/' ditaarch:DITAArchVersion='2.0' specializations='@props/audience @props/deliveryTarget @props/otherprops @props/platform @props/product' class='- topic/topic ' id='task' outputclass='task'>");
+        buf.append("<title class='- topic/title '>Task</title>");
+        buf.append("<body class='- topic/body '>");
+        for (int i = 0; i < context; i++) {
+            buf.append("<p class='- topic/p '>Context</p>");
+        }
+        buf.append("<ol class='- topic/ol '>");
+        buf.append("<li class='- topic/li '>");
+        buf.append("<p class='- topic/p '>");
+        if (inline > 0) {
+            if (inline == 1) {
+                buf.append("head");
+            }
+            buf.append("<b class='+ topic/ph hi-d/b '>");
+        }
+        buf.append("Command");
+        if (inline > 0) {
+            buf.append("</b>");
+            if (inline == 2) {
+                buf.append("tail");
+            }
+        }
+        buf.append("</p>");
+        for (int i = 0; i < info; i++) {
+            buf.append("<p class='- topic/p '>Info.</p>");
+        }
+        buf.append("</li>");
+        buf.append("</ol>");
+        buf.append("</body>");
+        buf.append("</topic>");
+
+        return new StringReader(buf.toString());
+    }
+
+    private Reader generateExp(int context, int info, int inline) {
+        final StringBuilder buf = new StringBuilder();
+        buf.append("<task xmlns:ditaarch='http://dita.oasis-open.org/architecture/2005/' ditaarch:DITAArchVersion='2.0' specializations='@props/audience @props/deliveryTarget @props/otherprops @props/platform @props/product' class='- topic/topic task/task ' id='task'>");
+        buf.append("<title class='- topic/title '>Task</title>");
+        buf.append("<taskbody class='- topic/body task/taskbody '>");
+        if (context > 0) {
+            buf.append("<context class='- topic/section task/context '>");
+            for (int i = 0; i < context; i++) {
+                buf.append("<p class='- topic/p '>Context</p>");
+            }
+            buf.append("</context>");
+        }
+        buf.append("<steps class='- topic/ol task/steps '>");
+        buf.append("<step class='- topic/li task/step '>");
+        buf.append("<cmd class='- topic/ph task/cmd '>");
+        if (inline > 0) {
+            if (inline == 1) {
+                buf.append("head");
+            }
+            buf.append("<b class='+ topic/ph hi-d/b '>");
+        }
+        buf.append("Command");
+        if (inline > 0) {
+            buf.append("</b>");
+            if (inline == 2) {
+                buf.append("tail");
+            }
+        }
+        buf.append("</cmd>");
+        if (info > 0) {
+            buf.append("<info class='- topic/itemgroup task/info '>");
+            for (int i = 0; i < info; i++) {
+                buf.append("<p class='- topic/p '>Info.</p>");
+            }
+            buf.append("</info>");
+        }
+        buf.append("</step>");
+        buf.append("</steps>");
+        buf.append("</taskbody>");
+        buf.append("</task>");
+
+        return new StringReader(buf.toString());
     }
 }
