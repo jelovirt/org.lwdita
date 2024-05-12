7ebdee78c87e81f043bba7f81ae433f0de0789f9
Jarno Elovirta <jarno@elovirta.com>
2024-05-12 12:29:59 +0300

    Disable Wiki support by default in Markdown
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/test/java/com/elovirta/dita/markdown/MarkdownReaderTest.java
+++ src/test/java/com/elovirta/dita/markdown/MarkdownReaderTest.java
@@ -136,26 +136,21 @@ public class MarkdownReaderTest extends AbstractReaderTest {
         new MutableDataSet()
           .set(Parser.EXTENSIONS, singletonList(YamlFrontMatterExtension.create()))
           .set(DitaRenderer.FIX_ROOT_HEADING, true)
+          .set(DitaRenderer.ID_FROM_YAML, true)
+          .set(DitaRenderer.WIKI, true)
       );
     final TestErrorHandler errorHandler = new TestErrorHandler();
     reader.setErrorHandler(errorHandler);
 
     run(getSrc() + file, getExp() + "wiki/" + file.replaceAll("\\.md$", ".dita"));
 
-    assertEquals(1, errorHandler.warnings.size());
-    final SAXParseException e = errorHandler.warnings.get(0);
-    assertEquals("Document content doesn't start with heading", e.getMessage());
-    assertEquals("classpath:/markdown/" + file, e.getSystemId());
-    assertNull(e.getPublicId());
-    assertEquals(1, e.getLineNumber());
-    assertEquals(1, e.getColumnNumber());
+    assertEquals(0, errorHandler.warnings.size());
   }
 
   @ParameterizedTest
   @ValueSource(strings = { "missing_root_header.md", "missing_root_header_with_yaml.md" })
   public void test_emptyHeader(String file) throws Exception {
-    reader =
-      new MarkdownReader(new MutableDataSet().set(Parser.EXTENSIONS, singletonList(YamlFrontMatterExtension.create())));
+    reader = new MarkdownReader();
     final TestErrorHandler errorHandler = new TestErrorHandler();
     reader.setErrorHandler(errorHandler);
 
