b0609905debc2c10cdc8482204f69047b2f86224
Jarno Elovirta <jarno@elovirta.com>
2024-08-18 19:59:06 +0300

    Add support for codeblock language prefix
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/main/java/com/elovirta/dita/markdown/DitaRendererOptions.java
+++ src/main/java/com/elovirta/dita/markdown/DitaRendererOptions.java
@@ -6,9 +6,11 @@ public class DitaRendererOptions {
 
   public final boolean doNotRenderLinksInDocument;
   public final String noLanguageClass;
+  public final String languageClassPrefix;
 
   public DitaRendererOptions(DataHolder options) {
     doNotRenderLinksInDocument = DitaRenderer.DO_NOT_RENDER_LINKS.get(options);
     noLanguageClass = DitaRenderer.FENCED_CODE_NO_LANGUAGE_CLASS.get(options);
+    languageClassPrefix = DitaRenderer.FENCED_CODE_LANGUAGE_CLASS_PREFIX.get(options);
   }
 }
